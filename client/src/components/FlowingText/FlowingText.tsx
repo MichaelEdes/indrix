99adabe7f3b8b363787eed286925ef1e02c5d2f0
Michael Edes <michael.edes@boxmodel.com>
2023-05-17 15:11:44 +0100

    chore: add flowing text basic layout + styling fix

--- client/src/components/FlowingText/FlowingText.tsx
+++ client/src/components/FlowingText/FlowingText.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import "./FlowingText.css";
 
 function FlowingText() {
-  const content = [];
   return (
-    <div>
-      <h1>Floating Text</h1>
+    <div className="flowing-text-container">
+      <h2>Live. Forever.</h2>
+      <div className="flowing-text">
+        <h1>
+          "Indrex isn't just about prolonging life - it's about enriching the
+          human experience beyond natural limits. Our mission is to fuse the
+          timeless essence of humanity with the endless possibilities of
+          technology. We believe in a future where death is not an end but a new
+          beginning, a leap into a new frontier of existence." <br />
+          <br /> - Singrid Indrex
+        </h1>
+      </div>
     </div>
   );
 }
--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import PageHero from "../../components/PageHero/PageHero";
 import "./HomePage.css";
 import ModelCarousel from "../../components/ModelCarousel/ModelCarousel";
+import FlowingText from "../../components/FlowingText/FlowingText";
 
 function HomePage() {
   return (
     <div className="home-page-container">
       <PageHero />
+      <div>
+        <FlowingText />
+      </div>
       <div className="model-carousel-container">
         <ModelCarousel />
       </div>
